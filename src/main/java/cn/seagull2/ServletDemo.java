6189a738143351064aacc14a7ac0fe4c407f05b0
徐云金YunjinXu <xuyunjin@foxmail.com>
2023-11-30 17:37:26 +0800

    增加ServletDemo

---
+++ src/main/java/cn/seagull2/ServletDemo.java
@@ -0,0 +1,25 @@
+package cn.seagull2;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletDemo extends HttpServlet {
+
+    public void init() {
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println(request.getRequestURI());
+        out.println(request.getContextPath());
+        out.println(request.getRequestURL().toString());
+        out.println(request.getQueryString());
+        out.println(request.getHeader("referer"));
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.doGet(request, response);
+    }
+}
